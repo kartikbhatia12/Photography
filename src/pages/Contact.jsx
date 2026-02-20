775c9a9c0eaa5d734b7d8c9bdd8ca042d703df96
Kartik Bhatia <218950101+kartikbhatia12@users.noreply.github.com>
2026-02-20 13:17:40 +0530

    Add files via upload

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,18 @@
+import ContactForm from "../components/ContactForm"
+import Footer from "../components/Footer"
+import Navbar from "../components/Navbar"
+import WhatsAppButton from "../components/WhatsappButton"
+
+
+const Contact = () => {
+    return (
+        <>
+          <Navbar />
+          <ContactForm />
+          <Footer />
+          <WhatsAppButton />
+        </>
+    )
+}
+
+export default Contact;
